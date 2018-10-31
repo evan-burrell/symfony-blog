2986220dd4aaf223c449781b79e2a916f7c09715
Evan Burrell <evan.burrell@ampersandit.co.uk>
2018-10-31 12:48:54 +0000

    CRUD and showing only comments per post

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -5,24 +5,22 @@ namespace App\Form;
 use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
-class CreatePost extends AbstractType
+class PostType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title', TextType::class)
-            ->add('body', TextType::class);
+            ->add('title')
+            ->add('body')
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefaults(array(
+        $resolver->setDefaults([
             'data_class' => Post::class,
-        ));
+        ]);
     }
 }

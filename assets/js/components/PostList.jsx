6c7eac947c973bda6ea914cb3a38e06546b9c1bd
Evan Burrell <evan.burrell@ampersandit.co.uk>
2018-11-06 08:30:06 +0000

    Add hooks React 16.7-alpha

--- assets/js/components/PostList.jsx
+++ assets/js/components/PostList.jsx
@@ -1,48 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Post from './Post';
 import Loading from 'react-simple-loading';
 
-export default class PostList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { posts: null };
-  }
+export default function PostList() {
+  const [posts, setPosts] = useState([]);
 
-  async getPosts() {
+  useEffect(async () => {
     const url = 'http://symfony-blog.ampdev.co/api/posts';
     const response = await axios.get(url);
-    return response.data;
-  }
+    setPosts(response.data);
+  }, []);
 
-  componentDidMount() {
-    if (!this.state.posts) {
-      (async () => {
-        try {
-          this.setState({
-            posts: await this.getPosts()
-          });
-        } catch (e) {
-          console.log(e);
-        }
-      })();
-    }
-  }
-
-  render() {
-    return this.state.posts ? (
-      this.state.posts.map((post, i) => (
-        <Post
-          key={i}
-          id={post[0].id}
-          title={post[0].title}
-          email={post.email}
-        />
-      ))
-    ) : (
-      <div>
-        <Loading />
-      </div>
-    );
-  }
+  return posts ? (
+    posts.map((post, i) => (
+      <Post key={i} id={post[0].id} title={post[0].title} email={post.email} />
+    ))
+  ) : (
+    <div>
+      <Loading />
+    </div>
+  );
 }

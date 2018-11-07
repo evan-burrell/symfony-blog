f6af56ed4fed8eb56b576d05672ef2cefc0810a2
Evan Burrell <evan.burrell@ampersandit.co.uk>
2018-11-07 10:03:25 +0000

    Purge CSS for tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,17 @@
-let tailwindcss = require('tailwindcss');
+const tailwindcss = require('tailwindcss')
+const purgecss = require('@fullhuman/postcss-purgecss')
+const cssnano = require('cssnano')
+const autoprefixer = require('autoprefixer')
 
- module.exports = {
-     plugins: [
-         tailwindcss('tailwind.js'), // your tailwind.js configuration file path
-         require('autoprefixer'),
-     ]
- }
+module.exports = {
+  plugins: [
+    tailwindcss('./tailwind.js'),
+    cssnano({
+      preset: 'default',
+    }),
+    purgecss({
+      content: ['./templates/**/*.twig', './templates/**/**/*.twig', './assets/js/*.js', './assets/js/**/*.jsx' ]
+    }),
+    autoprefixer
+  ]
+}

4bee1e797aeb9e41644c97aeee920673b9aae20e
Evan Burrell <evan.burrell@ampersandit.co.uk>
2018-11-07 14:21:59 +0000

    Spelling errors

--- __tests__/noAuthNavbar.spec.js
+++ __tests__/noAuthNavbar.spec.js
@@ -5,15 +5,15 @@ import NoAuthNavbar from '../assets/js/components/NoAuthNavbar';
 const wrapper = shallow(<NoAuthNavbar />);
 
 describe('<NoAuthNavbar />', () => {
-  it('renders three <div> in <NoAuthNavar />', () => {
+  it('renders three <div> in <NoAuthNavbar />', () => {
     expect(wrapper.find('div')).toHaveLength(3);
   });
 
-  it('renders home in <NoAuthNavar />', () => {
+  it('renders home in <NoAuthNavbar />', () => {
     expect(wrapper.text()).toContain('Home');
   });
 
-  it('renders login in <NoAuthNavar />', () => {
+  it('renders login in <NoAuthNavbar />', () => {
     expect(wrapper.text()).toContain('Login');
   });
 });
